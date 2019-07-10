83d544308daff13a0e254fa76657c77677fbe8cb
Haris Dzidic <haris@mop.ba>
2019-07-10 13:38:05 +0200

    :wq Init

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+it('works', async () => {
+  const ui = <App />;
+  const { container, getByLabelText, getByRole } = render(ui);
+  fireEvent.drop(getByLabelText('Upload files'), {
+    target: { files: [new File([''], 'filename', { type: 'image/png' })] }
+  });
+  await flushPromises(ui, container);
+  expect(getByRole('alert')).toHaveTextContent('1');
 });
+
+it('works?', async () => {
+  const ui = <App />;
+  const { container, getByLabelText, getByRole } = render(ui);
+  fireEvent.drop(getByLabelText('Upload files'), {
+    target: { files: [new File([''], 'filename', { type: 'image/png' }), new File([''], 'filename2', { type: 'video/mp4' })] }
+  });
+  await flushPromises(ui, container);
+  expect(getByRole('alert')).toHaveTextContent('2');
+});
+
+
+it('works?', async () => {
+  const ui = <App />;
+  const { container, getByLabelText, getByRole } = render(ui);
+  fireEvent.drop(getByLabelText('Upload files'), {
+    target: { files: [new File([''], 'filename', { type: 'image/png' }), new File([''], 'filename2', { type: 'video/mp4' }), new File([''], 'filename2', { type: 'video/mp4' })] }
+  });
+  await flushPromises(ui, container);
+  expect(getByRole('alert')).toHaveTextContent('3');
+});
+
+
+it('works?', async () => {
+  const ui = <App />;
+  const { container, getByLabelText, getByRole } = render(ui);
+  fireEvent.drop(getByLabelText('Upload files'), {
+    target: { files: [new File([''], 'filename', { type: 'image/png' }), new File([''], 'filename2', { type: 'video/mp4' }), new File([''], 'filename2', { type: 'video/mp4' }), new File([''], 'filename2', { type: 'video/mp4' })] }
+  });
+  await flushPromises(ui, container);
+  expect(getByRole('alert')).toHaveTextContent('4');
+});
+
+function flushPromises(ui, container) {
+  return new Promise(resolve =>
+    setImmediate(() => {
+      render(ui, { container })
+      resolve(container)
+    })
+  )
+}
